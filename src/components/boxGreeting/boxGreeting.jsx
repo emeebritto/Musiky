662917e02ee9853a2caa9bc7f75bc3a5d67a4fa4
Emerson_Britto <emersonbritto987@gmail.com>
2021-08-18 10:56:21 -0300

    greeting

--- src/components/boxGreeting/boxGreeting.jsx
+++ src/components/boxGreeting/boxGreeting.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Styled from "styled-components"
 import { Link } from 'react-router-dom'
+import closeIcon from '../../assets/icons/close_white_24dp.svg'
 
 
 const ViewPort = Styled.section`
@@ -12,9 +13,20 @@ const ViewPort = Styled.section`
     width: 100%;
     height: 100px;
     margin-bottom: 35px;
-    opacity: 80%;
     background: url(https://www.enjpg.com/img/2020/aesthetic-desktop-3.png) no-repeat center/100% black;
     background-position: 0% 70%;
+`
+const GreetingText = Styled.h1`
+    color: white;
+    font-size: 1.6em;
+    margin-left: 50px;
+    background-color: rgb(0 0 0 / 30%);
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    padding: 5px 10px;
+`
+const BtnClose = Styled.img`
+    margin-right: 25px;
+    cursor: pointer;
 `
 
 
@@ -23,7 +35,8 @@ const BoxGreeting = () => {
     return (
         <>
             <ViewPort>
-                <h1>Good Evening</h1>
+                <GreetingText>Good Evening, Emerson Britto</GreetingText>
+                <BtnClose src={closeIcon} alt='close button'/>
             </ViewPort>
         </>
     )
