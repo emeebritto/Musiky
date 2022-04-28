d208bc8509ee1c73eaa673f099e35f4235e363a4
Emerson-Britto <emersonbritto987@gmail.com>
2022-04-28 00:31:16 -0300

    formatEnumarals function fixes

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,15 +2,16 @@ import { formatNumerals } from 'format-numerals';
 import { Obj } from 'common/types';
 
 
-export const formatValues = (value:number): string => {
-	let formatedValue: string[] = formatNumerals(value, { comma: 2 }).split(',');
+export const formatValues = (value:number | string): string => {
+	const valueStr = String(value);
+	const formatedValue: string[] = formatNumerals(parseInt(valueStr), { comma: 2 }).split(',');
 
 	if (formatedValue.length === 3) return `${formatedValue[0]}.${formatedValue[1][1]}M`;
 	if (formatedValue.length === 2) return `${formatedValue[0]}.${formatedValue[1][1]}K`;
 	return formatedValue[0];
 };
 
-export const mapObjValues = (obj:Obj | undefined, type:(s:Obj) => any) => {
+export const mapObjValues = (obj:Obj | undefined, type:(s:any) => any) => {
 	if (!obj) return {};
 	const newObj:Obj = {};
 	for (const [key, value] of Object.entries(obj)) {
